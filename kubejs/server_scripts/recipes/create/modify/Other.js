9d3a66acde6dd2c0c764afb833ab46f8cde02a62
Qi-Month <dy757410@163.com>
2025-04-21 21:43:17 +0800

    add Moon Cobalt Ore modify part recipes
    other plz see UpdateLogs

--- kubejs/server_scripts/recipes/create/modify/Other.js
+++ kubejs/server_scripts/recipes/create/modify/Other.js
@@ -27,8 +27,13 @@ ServerEvents.recipes((event) => {
 	// 烈焰蛋糕
 	create.filling("create:blaze_cake", [
 		"create:blaze_cake_base",
-		Fluid.of("tconstruct:blazing_blood", 500)
+		Fluid.of("minecraft:lava", 500)
 	]).id("create:filling/blaze_cake")
+
+	create.filling("2x create:blaze_cake", [
+		"create:blaze_cake_base",
+		Fluid.of("tconstruct:blazing_blood", 500)
+	])
 
 	// 烈焰蛋糕胚
 	create.compacting("create:blaze_cake_base", [
