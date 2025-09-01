c1f4638b41cb48dfa11f04ad2f5f624a2c71100b
Qi-Month <dy757410@163.com>
2025-09-02 02:48:44 +0800

    一些调整

--- kubejs/startup_scripts/register/block/custom/!Function.js
+++ kubejs/startup_scripts/register/block/custom/!Function.js
@@ -3,12 +3,12 @@ let $Item$Properties = Java.loadClass("net.minecraft.world.item.Item$Properties"
 
 let customBlockDefs = []
 
-function defineCustomBlock(id, BlockClass, properties) {
+function defineCustomBlock(id, blockClass, properties) {
 	let newId = id.includes(":") ? id : `${global.namespace}:${id}`
 
 	customBlockDefs.push({
 		id: newId,
-		blockClass: BlockClass,
+		blockClass: blockClass,
 		props: properties
 	})
 }
