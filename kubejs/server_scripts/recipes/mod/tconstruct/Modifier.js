e58eb15b6a0e2324381df0cd46b92e5d5c834353
Qi-Month <dy757410@163.com>
2026-09-07 13:20:10 +0800

    配方

--- kubejs/server_scripts/recipes/mod/tconstruct/Modifier.js
+++ kubejs/server_scripts/recipes/mod/tconstruct/Modifier.js
@@ -455,6 +455,17 @@ ServerEvents.recipes((event) => {
 		])
 		.build("tconstruct:tools/modifiers/ability/unbreakable")
 
+	// 潜水
+	new ModifierRecipeBuilder("cmi:diving")
+		.allowCrystal(true)
+		.checkTraitLevel(true)
+		.level(1)
+		.tools("tconstruct:plate_helmet")
+		.inputs([
+			"#create:diving_helmets"
+		])
+		.build()
+
 	let luckRecipes = [
 		[1, "#forge:dyes/blue", true],
 		[2, "#forge:gems/diamond", false],
